3ad55428ed4e166906d1ea4362ac6a3601d7c4e9
Isabella Shevchuk <ishevchuk@griddynamics.com>
2023-09-04 11:29:40 +0200

    all features

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ from torch.nn.functional import normalize
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model, preprocess = clip.load("ViT-B/32")
 
-fashion_data = pd.read_csv('fashion_set.csv')
+fashion_data = pd.read_csv('fashion_set1.csv')
 
 def clean_and_convert(embedding_str):
     cleaned_str = embedding_str.strip("['").strip("']").replace("\n", "").replace("  ", " ")
@@ -65,7 +65,7 @@ def find_similar_images_to_text(text_query, category=None, k=3):
     query_text_features = calculate_text_features(text_query)
 
     similarity_scores = []
-    if category is not None:
+    if category != "all":
         filtered_data = fashion_data[fashion_data['category'] == category.lower()]
     else:
         filtered_data = fashion_data
@@ -85,10 +85,16 @@ def find_similar_images_to_text(text_query, category=None, k=3):
     similar_images_data = fashion_data.iloc[[index for index, _ in top_similar_images]]
     return similar_images_data, top_similar_images
 
-def find_similar_images_to_img(query_img_features, needed_category, num_similar=3):
-    category_data = fashion_data[fashion_data['category'] == needed_category]
-
+def find_similar_images_to_img(image, needed_category, num_similar=3):
+    image_pil = Image.fromarray(image)
+    query_img_features = calculate_image_features(image_pil)
     similarities = []
+    
+    if needed_category != "all":
+        category_data = fashion_data[fashion_data['category'] == needed_category]
+    else:
+        category_data = fashion_data
+
     for index, row in category_data.iterrows():
         row_img_features = torch.tensor(row['image_features'], device=device, dtype=torch.float32)
 
@@ -99,6 +105,35 @@ def find_similar_images_to_img(query_img_features, needed_category, num_similar=
     similar_images = similarities[:num_similar]
 
     return similar_images
+
+def find_similar_images_for_file_and_text(image, text_query, category=None, k=3, alpha=0.65):
+    image_pil = Image.fromarray(image)
+    query_img_features = calculate_image_features(image_pil)
+
+    query_text_features = calculate_text_features(text_query)
+
+    similarity_scores = []
+    if category != "all":
+        filtered_data = fashion_data[fashion_data['category'] == category]
+    else:
+        filtered_data = fashion_data
+
+    for index, row in filtered_data.iterrows():
+        row_img_features = torch.tensor(row['image_features'], device=device, dtype=torch.float32)
+
+        similarity = alpha * torch.dot(query_text_features.squeeze(), row_img_features.squeeze()) + \
+            (1 - alpha) * torch.dot(query_img_features.squeeze(), row_img_features.squeeze())
+        
+        similarity_scores.append((index, similarity.item()))
+
+    similarity_scores.sort(key=lambda x: x[1], reverse=True)
+    top_similar_images = similarity_scores[:k]
+
+    similar_images_data = fashion_data.loc[[index for index, _ in top_similar_images]]
+    top_similarity_scores = [score for _, score in top_similar_images]
+    similar_images_data['similarity_score'] = top_similarity_scores
+    
+    return similar_images_data
 
 def catalog(category):
     items = fashion_data[fashion_data['category'] == category.lower()]
@@ -128,11 +163,8 @@ def search_by_text(text, category):
     return images_with_captions
 
 def search_by_image(image, category):
-    image_pil = Image.fromarray(image)
-    query_img_features = calculate_image_features(image_pil)
-    
     category = category.lower()
-    similar_images = find_similar_images_to_img(query_img_features, category)
+    similar_images = find_similar_images_to_img(image, category)
     images_with_captions = []
 
     for img_path, _ in similar_images:
@@ -142,37 +174,45 @@ def search_by_image(image, category):
 
     return images_with_captions
 
-
 def search_by_image_and_text(text, image, category, alpha):
-    d = (alpha * text + (1-alpha) * image) + category
-    return d
+    similar_images_data = find_similar_images_for_file_and_text(image, text, category.lower(), alpha=alpha)
+    
+    images_with_captions = []
+
+    for _, row in enumerate(similar_images_data.iterrows()):
+        img_path = row[1]['full_path']
+        img = Image.open(img_path)
+        caption = f"{row[1]['description']}"
+        images_with_captions.append((img, caption))
+
+    return images_with_captions
 
 with gr.Blocks() as demo:
     gr.Markdown("Some features that can be done using CLIP")
     with gr.Tab("Catalog"):
         catalog_dropdown_input = gr.Dropdown(categories0, label="Choose needed category please")
-        catalog_image_output_plot = gr.Gallery()
         catalog_button = gr.Button("Show")
+        catalog_image_output_plot = gr.Gallery(preview=True)
 
     with gr.Tab("Search by Text description"):
         sbt_text_input = gr.Textbox(label="Add text description of interested item please")
         sbt_dropdown_input = gr.Dropdown(categories, label="Choose needed category please")
-        sbt_image_output = gr.Gallery()
         sbt_button = gr.Button("Show")
+        sbt_image_output = gr.Gallery(preview=True)
 
     with gr.Tab("Similar to input img"):
         sti_image_input = gr.Image()
         sti_dropdown_input = gr.Dropdown(categories, label="Choose needed category please")
-        sti_image_output = gr.Gallery()
         sti_button = gr.Button("Show")
+        sti_image_output = gr.Gallery(preview=True)
 
     with gr.Tab("Similar to input img and text"):
         img_and_txt_text_input = gr.Textbox(label="Add text description of interested item please")
         img_and_txt_image_input = gr.Image()
         img_and_txt_dropdown_input = gr.Dropdown(categories, label="Choose needed category please")
         img_and_txt_slider = gr.Slider(0, 1, label="How much text feature is more important than image feature?")
-        img_and_txt_image_output = gr.Image()
         img_and_txt_button = gr.Button("Show")
+        img_and_txt_image_output = gr.Gallery(preview=True)
 
     with gr.Accordion("Open for More!"):
         gr.Markdown("How to use")
